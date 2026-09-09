cbabcbf82d250ab7fe259650c53c4a91b4623d7f
demin6632-del <demin6632@gmail.com>
2026-09-09 09:36:12 +0500

    Create MainActivity.kt

---
+++ app/src/main/java/com/chronicles/abyss/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.chronicles.abyss
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+
+        webView.webViewClient = WebViewClient()
+
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+
+        webView.loadUrl("file:///android_asset/game.html")
+
+        setContentView(webView)
+    }
+}
